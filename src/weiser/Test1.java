ca4820fdf6b2556522d042608017822200711921
David Langheiter <david@langheiter.com>
2018-01-30 10:51:41 +0100

    Changed Test1.java

--- src/weiser/Test1.java
+++ src/weiser/Test1.java
@@ -5,6 +5,11 @@ public class Test1 {
 	public static void halloVonWJ() {
 		System.out.println("Hallo von Johann Weiser!");
 	}
+	
+	public static void halloVonD() {
+		System.out.println("Hallo von David Langheiter!");
+	}
+	
 	public static void main(String[] args) {
 		halloVonWJ();
 	}
