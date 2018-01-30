78431b1d3b5495f70ed7ad9431b5a1f9046c5481
Johann Weiser <johann.weiser@aon.at>
2018-01-30 10:47:31 +0100

    First commit.

---
+++ src/weiser/Test1.java
@@ -0,0 +1,12 @@
+package weiser;
+
+public class Test1 {
+
+	public static void halloVonWJ() {
+		System.out.println("Hallo von Johann Weiser!");
+	}
+	public static void main(String[] args) {
+		halloVonWJ();
+	}
+
+}
